75e17ba6725c58bd67eee88dd4f2da00dd402164
jonas089 <jonaspauli089@gmail.com>
2020-03-05 11:27:53 +0100

    05.03.2020 | 11:27

--- chain.py
+++ chain.py
@@ -1,9 +1,25 @@
+from validate import *
 import hashlib
 import time
+import pickle
 
 LocalChain = []
 TxData = []
 blocktime = 10
+
+class Localchain:
+    def BLOCKCHAINDAT():
+        try:
+            open('data/blockchain.dat', 'x')
+        except Exception as Exists:
+            print(Exists)
+            pass
+    def WALLETDAT():
+        try:
+            open('data/wallet.dat', 'x')
+        except Exception as Exists:
+            print(Exists)
+            pass
 
 class BLOCKCHAIN:
     def BLOCK():
@@ -31,12 +47,12 @@ class BLOCKCHAIN:
             next_timestamp = timestamp + blocktime
             #[HASHING]
         sha = hashlib.sha256()
-        current_block_data_string = str(index) + prev_hash
+        current_block_data_string = str(index) + prev_hash + str(timestamp)
         current_block_hash_string = sha.update(current_block_data_string.encode('utf-8'))
         #[BLOCKVAR] 5                                       block_hash
         block_hash = str(sha.hexdigest())
         sha = hashlib.sha256()
-        next_block_data_string = str(index + 1) + block_hash
+        next_block_data_string = str(index + 1) + block_hash + str(next_timestamp)
         next_block_hash_string = sha.update(next_block_data_string.encode('utf-8'))
         #[BLOCKVAR] 6                                       next_block_hash
         next_block_hash = str(sha.hexdigest())
@@ -55,13 +71,36 @@ class BLOCKCHAIN:
 #[END OF BLOCK DICTIONARY]
         LocalChain.append(index)
         LocalChain[index] = Block
+        if Validation.VALIDATE(LocalChain) == True:
+            with open('data/blockchain.dat', 'wb') as chaindatafile:
+                pickle.dump(LocalChain, chaindatafile)
+        else:
+            return False
+
         return True
+
+def GENERATEGENESIS():
+    while len(LocalChain) == 0:
+        BLOCKCHAIN.BLOCK()
+    print('[GENESIS]' + '\n' + '-' * 30 + '\n' + str(LocalChain) + '\n' + '-' * 30 + '\n')
+    with open('data/blockchain.dat', 'wb') as chaindatafile:
+        pickle.dump(LocalChain, chaindatafile)
+
+
+Localchain.BLOCKCHAINDAT()
+Localchain.WALLETDAT()
 
 
 #[CALL FUNCTIONS FOR TESTING || DEBUG]
-for routine in range(0, 50): # Blocktime was changed to 10 seconds ==> running 50 times with a sleep timer will generate 5 Blocks
-    BLOCKCHAIN.BLOCK()
-    time.sleep(1)
-for block in range(0, len(LocalChain)):
-    print('[BLOCK]' + '\n' + '----------' + '\n' + str(LocalChain[block]) + '\n' + '---------' + '\n')
+debugI = input('[Genesis] = Generate Genesis Block; [Generate] = Generate 5 Blocks: ')
+if debugI == 'Generate':
+    with open('data/blockchain.dat', 'rb') as chaindatafile:
+        LocalChain = pickle.load(chaindatafile)
+    for routine in range(0, 50): # Blocktime was changed to 10 seconds ==> running 50 times with a sleep timer will generate 5 Blocks
+        BLOCKCHAIN.BLOCK()
+        time.sleep(1)
+    for block in range(0, len(LocalChain)):
+        print('[BLOCK]' + '\n' + '----------' + '\n' + str(LocalChain[block]) + '\n' + '---------' + '\n')
 #[END OF DEBUGGING SECTION]
+if debugI == 'Genesis':
+    GENERATEGENESIS()
--- validate.py
+++ validate.py
@@ -1,1 +1,53 @@
-#SOON
+import hashlib
+# blockstructure : index, prev_hash, timestamp, next_timestamp, block_hash, next_block_hash
+# blockvalues (hashed) : index, prev_hash, timestamp
+def CHECKPOINTS():
+	checkpoints = []
+	checkpoints.append(0)
+	checkpoints[0] = {}
+	checkpoints[0]['index'] = 0
+	checkpoints[0]['hash'] = '' # insert Genesis hash
+	checkpoints[0]['next_hash'] = '' # insert Hash following Genesis hash
+	return checkpoints
+class Validation:
+	def VALIDATE(Block_Chain):
+		for block_index in range(0, len(Block_Chain) - 1):
+			index = Block_Chain[block_index]['index']
+			prev_hash = Block_Chain[block_index]['prev_hash']
+			timestamp = Block_Chain[block_index]['timestamp']
+			next_timestamp = Block_Chain[block_index]['next_timestamp']
+			block_hash = Block_Chain[block_index]['block_hash']
+			next_block_hash = Block_Chain[block_index]['next_block_hash']
+
+			# validation of <block_hash>
+			data_string = str(index) + prev_hash + str(timestamp)
+			sha = hashlib.sha256()
+			reconstructed_block_hash = sha.update(data_string.encode('utf-8'))
+			reconstructed_block_hash_string = str(sha.hexdigest())
+			if reconstructed_block_hash_string != block_hash:
+				print('[E1]')
+				return False
+
+			# validation of <next_block_hash>
+			next_data_string = str(index + 1) + block_hash + str(next_timestamp)
+			sha = hashlib.sha256()
+			reconstructed_next_block_hash = sha.update(next_data_string.encode('utf-8'))
+			reconstructed_next_block_hash_string = str(sha.hexdigest())
+			if reconstructed_next_block_hash_string != next_block_hash:
+				print('[E2]')
+				return False
+
+			# validation of <index>
+			if block_index == 0 and index != 0:
+				# Genesis index invalid
+				print('[E3]')
+				return False
+
+			if block_index > 0 and index > 0:
+				if index != Block_Chain[block_index - 1]['index'] + 1:
+					# Block index invalid
+					print('[E4]')
+					return False
+		# when every block was confirmed, return True ==> chain is valid !
+		# Genesis should probably be a checkpoint...
+		return True
