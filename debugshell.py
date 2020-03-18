337640d6a61342206d337f59bb3936f3eab50000
Rom3dius <rom3dius@yahoo.com>
2020-03-18 12:54:11 +0100

    Improved debugshell.py

--- debugshell.py
+++ debugshell.py
@@ -1,10 +1,17 @@
-import chain
-import validation
+from flask import Flask, request, jsonify
+import argparse
 import account
-import transaction
-import sys
-import code
+import chain
+import json
+import sync
 import pickle
+import threading
+import values
+#from threading import Thread
+from multiprocessing import Process, Value
+import validation
+import time
+import code
 
 def ahelp():
     print("""
@@ -12,7 +19,14 @@ def ahelp():
     New Account --> newacc()
     Generate Block --> genblo()
     Balance --> bal()
+    Printchain --> printchain(index of block)
+    Node --> startnode()
     """)
+
+def printchain(num=0):
+    array = chain.LOADLOCALCHAIN()
+    print(array[num])
+
 
 def gengen():
     LocalChain = chain.LOADLOCALCHAIN()
@@ -32,7 +46,75 @@ def bal():
         address = pickle.load(AccountFile)[0]
     print(account.LoadBalance(address))
 
+def startnode():
+    node = Flask(__name__)
+    account.__Start__()
+
+    @node.route('/blockchain.json', methods = ['GET'])
+    def ReturnLocalBlockchain():
+        BlockChain = chain.LOADLOCALCHAIN()
+        BlockChainDict = {}
+        BlockChainDict['data'] = BlockChain
+        return BlockChainDict
+
+    @node.route('/txpool.json', methods = ['GET'])
+    def ReturnTxPool():
+        LocalChain = chain.LOADLOCALCHAIN()
+        TransactionPoolDict = {}
+        try:
+            next_index = LocalChain[len(LocalChain) - 1]['index'] + 1
+        except Exception as NoChain:
+            return TransactionPoolDict
+        local_txpool = []
+        try:
+            with open('src/TxBlockNo' + '000' + str(next_index) + '.dat', 'rb') as Transaction_Data_File:
+                local_txpool = pickle.load(Transaction_Data_File)
+        except Exception as no_data:
+            pass
+        TransactionPoolDict['data'] = local_txpool
+        return TransactionPoolDict
+
+
+    #@node.route('/block', methods=['POST'])
+    #def ReceiveBlock():
+    #    block_jsonified = json.loads(request.data)
+    #    print(block_jsonified)
+    #    return ('DONE')
+
+    #@node.route('/syncnetwork', methods=['GET'])
+    #def SyncNetwork():
+    #    sync.sync_thread()
+    #    return ('DONE')
+
+    @node.route('/transaction', methods=['POST'])
+    def ReceiveTransaction():
+        #transaction_decoded = request.data.decode('utf-8')
+        transaction_jsonified = request.get_json()#json.loads(request.data)
+        #print(transaction_jsonified)
+        result = validation.ValidationClass.VALIDATE_TRANSACTION(transaction_jsonified)
+        print('[TRANSACTION RECEIVED] : [VALID = ' + str(result) + ' ]')
+        return(str(result))
+
+
+
+    if __name__ == '__main__':
+        try:
+            open('debug.log', 'x')
+        except Exception as exists:
+            pass
+
+        parser = argparse.ArgumentParser(description='AMPS Node')
+        parser.add_argument('--port', '-p', default=str(values.rpc),
+                        help='port')
+        args = parser.parse_args()
+        process_var = Value('b', True)
+        # this had to be moved above node.run as it otherwise only gets called when the connection breaks
+        p = Process(target=sync.sync_thread, args=(process_var, ))
+        p.start()
+        node.run(threaded=True, host=values.ip, port=args.port, use_reloader=False)
+        p.join()
+
+
 print("Use ahelp() for debug help and help() for python help!")
 ahelp()
-
 code.interact(local=locals())
